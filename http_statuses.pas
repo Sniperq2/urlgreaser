c3a72366878fa2df8061fbcc40460c5c300ef88d
Sniperq2 <ogldelphi@mail.ru>
2023-04-19 22:19:23 +0300

    Start adding http statuses constants

---
+++ http_statuses.pas
@@ -0,0 +1,21 @@
+unit http_statuses;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+const
+  StatusOk = 200;
+
+  StatusForbidden = 403;
+  StatusNotFound = 404;
+
+  StatusInternalServerError = 500;
+
+implementation
+
+end.
+
